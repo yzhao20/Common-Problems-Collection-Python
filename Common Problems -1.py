d92cfc945ae99f218498832998504a75c281870b
Yunfeng <yzhao20@yahoo.com>
2020-10-13 10:00:38 +1100

    Update Common Problems -1.py
    10 Problems totally.

--- Common Problems -1.py
+++ Common Problems -1.py
@@ -1,7 +1,8 @@
 """
-1. List comprehension
-Syntax: 
-expression for item in list.
+1. Define or create a new list based on exsiting list(or string, or something is iterable)
+Solution: List comprehension
+Syntax: expression for item in list.
+Takeaway:
 """
 a = ["math", 100, "him", "her"]
 b = [str(i) for i in a]   # The item a could be a string or tuple whichever it could be iterable.
@@ -9,7 +10,10 @@ c = [type(i) for i in b]
 print(c)  # The result is still a list, including 4 items are all <class 'str'>.
 
 """
-2. Lambda function
-Syntax: 
-lambda arguments: expression
+2. Return
+Solution: Lambda function
+Syntax: lambda arguments: expression
+Takeaway:
 """
+a = lambda x, y: x * y * 20 + x ** 2 + 10
+print(a(10, 20))
